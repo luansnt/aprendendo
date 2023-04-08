a1584f68916a707d6b4e08e4a35c9bf6db33acd4
Luan Santana <127178163+luansnt@users.noreply.github.com>
2023-04-08 12:03:41 -0300

    Add files via upload

--- testando/doc.js
+++ testando/doc.js
@@ -2,23 +2,28 @@ function aparecer(){
     const criaturaFeita = JSON.parse(localStorage.getItem('criaturaFeita'))
     
     var campo = document.getElementById('receber')
-    let botaoRevelar = document.getElementById('revelar')
+    var Status = document.getElementById('Status')
+    var carater = document.getElementById('caracter')
+    var lista = document.getElementById('lista')
+    const botaoRevelar = document.getElementById('revelar')
   
-      campo.innerHTML +=  `<p>Status da Criatura</p> <br> <ul class="status">`
-   
-      campo.innerHTML += `<li class="status">Pontos de Vida: ${criaturaFeita[1][0]}</li>`
-      campo.innerHTML += `<li class="status">Pontos de Energia: ${criaturaFeita[1][1]}</li>`
-      campo.innerHTML += `<li class="status">Pontos de Poder: ${criaturaFeita[1][2]}</li>`
-      campo.innerHTML += `<li class="status">Ataque Basico: ${criaturaFeita[1][2]}d8</li>`
-      campo.innerHTML+= "</ul>"
-
-      campo.innerHTML +=  "<br><p>Caracteristicas </p> <br> <ul>"
+      Status.innerHTML += `<p>Status da Criatura</p> <br> `
+      
+      Status.innerHTML += `<li class="status">Pontos de Vida: ${criaturaFeita[1][0]}</li>`
+      Status.innerHTML += `<li class="status">Pontos de Energia: ${criaturaFeita[1][1]}</li>`
+      Status.innerHTML += `<li class="status">Poder: ${criaturaFeita[1][2]}</li>`
+      Status.innerHTML += `<li class="status">Parâmetros: ${criaturaFeita[1][3]}</li>`
+      Status.innerHTML += `<li class="status">Ataque Básico: ${criaturaFeita[1][2]}d8</li>`
+      
+     Status.innerHTML += `<br>`
+     carater.innerHTML +=  `Caracteristicas`
 
       for (let index = 0; index < criaturaFeita[0].length; index++) {
-        campo.innerHTML += `<li> ${criaturaFeita[0][index]}</li>`
+        lista.innerHTML += `<li> ${criaturaFeita[0][index]}</li>`
           
       }
-      campo.innerHTML += "</ul>"
+
+      
     
   botaoRevelar.setAttribute('disabled','')
 
--- testando/script.js
+++ testando/script.js
@@ -1,6 +1,6 @@
 var dadosProcessados =[];
 
-fetch('./homunculario.csv')
+fetch('./homuncularioteste.csv')
 .then(response => response.text())
 .then(data => {
     const linhas = data.split("\n");
@@ -20,20 +20,20 @@ function escolherDificuldade(DificuldadeEscrita, pda){
     DificuldadeEscrita = DificuldadeEscrita.toLowerCase()
     if (DificuldadeEscrita== "facil"){
         QuantCaract = 3
-        diff = 3
-        EstatosCriatura = [20 + (pda*6) ,2 + pda,1]
+        diff = 7
+        EstatosCriatura = [20 + (pda*6) ,2 + pda,1 ,2]
     }else if(DificuldadeEscrita == "normal"){
         QuantCaract = 4
-        diff = 6
-        EstatosCriatura = [40 + (pda*8),4 + pda,2]
+        diff = 14
+        EstatosCriatura = [40 + (pda*10),4 + pda,2 ,3]
     }else if(DificuldadeEscrita == "dificil"){
         QuantCaract = 5
-        diff = 9
-        EstatosCriatura = [80 + (pda*12),8 + pda,3]
+        diff = 21
+        EstatosCriatura = [80 + (pda*12),8 + pda,3 ,4]
     }else if(DificuldadeEscrita == "extrema"){
         QuantCaract = 6
-        diff = 12
-        EstatosCriatura = [120 + (pda*16),12 + pda,4]
+        diff = 28
+        EstatosCriatura = [120 + (pda*20),12 + pda,4 ,5]
     }
     return [diff, QuantCaract]
 }
@@ -45,7 +45,7 @@ function Habis (diff){
     var Escolhido = linhaEscolhida[Math.floor(Math.random() * linhaEscolhida.length )]
 
     for(var i = 0; i < HabilidadesEscolhidas.length; i++){ 
-        while (HabilidadesEscolhidas[i] == Escolhido) {
+        while ((HabilidadesEscolhidas[i] == Escolhido) || (HabilidadesEscolhidas[i] == "")) {
         aleatorio = Math.floor(Math.random()*diff) +1
         linhaEscolhida = dadosProcessados[aleatorio]
         Escolhido = linhaEscolhida[Math.floor(Math.random() * linhaEscolhida.length )]
@@ -54,28 +54,25 @@ function Habis (diff){
 
     HabilidadesEscolhidas.push(Escolhido)
 }
- function aparecer(){
-    
-  console.log(HabilidadesEscolhidas)
- }
-
+ 
 function iniciar(){
     HabilidadesEscolhidas = []
-    EstatosCriatura = [0, 0, 0] //PV , PE, PODER
+    EstatosCriatura = [0, 0, 0, 0] //PV , PE, PODER, PARAMETRO
     diff = 0
     QuantCaract = 0
     
     var escritaDif = document.getElementById("dificuldade").value
-    var habilidadeExtra = parseInt(document.getElementById("pda").value)
+    var pda = parseInt(document.getElementById("pda").value)
    
-    var total = Math.floor(habilidadeExtra/6)
-    let criatura = escolherDificuldade(escritaDif, habilidadeExtra)
+    var total = Math.floor(pda/6)
+    let criatura = escolherDificuldade(escritaDif, pda)
 
-    while (HabilidadesEscolhidas.length < criatura[1] + total){
+    while (HabilidadesEscolhidas.length < criatura[1]){
         Habis(criatura[0])
     }
     EstatosCriatura[2] += total
-      
+    EstatosCriatura[3] += total
+
     const criaturaFeita = [HabilidadesEscolhidas, EstatosCriatura ]
     localStorage.setItem('criaturaFeita', JSON.stringify(criaturaFeita))
 
